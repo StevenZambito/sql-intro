492e6c6c0893b7e364e1e0a5cc1a59ef9bee7c63
StevenZambito <zambito.steven@icloud.com>
2021-01-21 08:52:30 -0500

    Completed Explorer Mode

--- queries.sql
+++ queries.sql
@@ -42,3 +42,37 @@
  DELETE FROM "Employees" WHERE "FullName" = 'Lazy Larry';
 
  ALTER TABLE "Employees" ADD COLUMN "ParkingSpot" VARCHAR(10);
+
+ CREATE TABLE "Departments" ("Id" SERIAL PRIMARY KEY,"DepartmentName" TEXT NOT NULL,"Building" TEXT);
+
+ALTER TABLE "Employees" ADD COLUMN "DepartmentId" INTEGER NULL REFERENCES "Departments" ("Id");
+
+CREATE TABLE "Products" ("Id" SERIAL PRIMARY KEY, "Price" FLOAT, "Name" TEXT, "Description" TEXT, "QuantityInStock" INT);
+
+CREATE TABLE "Orders" ("Id" SERIAL PRIMARY KEY, "OrderNumber" TEXT, "DatePlaced" TEXT, "Email" TEXT);
+
+CREATE TABLE "ProductOrders" ("Id" SERIAL PRIMARY KEY, "ProductId" INTEGER REFERENCES "Products" ("Id"), "OrderId" INTEGER REFERENCES "Orders" ("Id"));
+ALTER TABLE "ProductOrders" ADD COLUMN "OrderQuantity" INTEGER;
+
+INSERT INTO "Departments" ("DepartmentName", "Building") VALUES ('Development', 'Main');
+INSERT INTO "Departments" ("DepartmentName", "Building") VALUES ('Marketing', 'North');
+
+INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "IsPartTime", "PhoneExtension", "DepartmentId") VALUES ('Todd Smith', 40000, 'Programmer', false, 123, 1);
+INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "IsPartTime", "PhoneExtension", "DepartmentId") VALUES ('Barbara Ramsey', 80000, 'Manager', false, 234, 1);
+INSERT INTO "Employees" ("FullName", "Salary", "JobPosition", "IsPartTime", "PhoneExtension", "DepartmentId") VALUES ('Tom Jones', 32000, 'Admin', true, 456, 2);
+
+INSERT INTO "Products" ("Price", "Name", "Description", "QuantityInStock") VALUES (12.45, 'Widget', 'The Original Widget', 100);
+INSERT INTO "Products" ("Price", "Name", "Description", "QuantityInStock") VALUES (99.99	, 'Flowbee', 'Perfect for haircuts', 3);
+
+INSERT INTO "Orders" ("OrderNumber", "DatePlaced", "Email") VALUES ('X529', 'Jan 1st, 2020 at 4:55PM', 'person@example.com');
+
+INSERT INTO "ProductOrders" ("ProductId", "OrderId", "OrderQuantity") VALUES (1,1,3);
+INSERT INTO "ProductOrders" ("ProductId", "OrderId", "OrderQuantity") VALUES (2, 1, 2);
+
+SELECT "Employees"."FullName" FROM "Employees" JOIN "Departments" ON "Departments"."Id" = "Employees"."DepartmentId" WHERE "Departments"."Building" = 'North Side';
+SELECT "Employees"."FullName" FROM "Employees" JOIN "Departments" ON "Departments"."Id" = "Employees"."DepartmentId" WHERE "Departments"."Building" = 'East Side';
+SELECT "Employees"."FullName" FROM "Employees" JOIN "Departments" ON "Departments"."Id" = "Employees"."DepartmentId" WHERE "Departments"."Building" = 'Main';
+
+SELECT * FROM "Orders" JOIN "ProductOrders" ON "ProductOrders"."OrderId" = "Orders"."Id" JOIN "Products" ON "Products"."Id" = "ProductOrders"."ProductId" WHERE "ProductOrders"."ProductId" = 2;
+
+SELECT "OrderQuantity" FROM "ProductOrders" JOIN "Products" ON "ProductOrders"."ProductId" = "Products"."Id" JOIN "Orders" ON "ProductOrders"."OrderId" = "Orders"."Id" WHERE "Name" = 'Flowbee' AND "OrderNumber" = 'X529';
